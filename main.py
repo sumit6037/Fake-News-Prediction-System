526f8197b1714bf840f4311440060d24f428966e
unknown <sumitkumar9797@gmail.com>
2022-10-29 23:42:44 +0530

    make it better

---
+++ main.py
@@ -0,0 +1,31 @@
+from flask import Flask,render_template,request
+
+import pandas as pd
+import pickle
+
+app= Flask(__name__)
+data=pd.read_csv("fake_news.csv")
+
+fake_df=pickle.load(open("FakeNewsDF.pkl",'rb'))
+tfidf=pickle.load(open("tfidf_v.pkl",'rb'))
+model=pickle.load(open("LRegModel.pkl",'rb'))
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/detect',methods=['POST'])
+
+def detect():
+    title=request.form.get('title')
+    news_content=fake_df[fake_df['title'] == title]['content']
+    X=tfidf.transform(news_content).toarray()
+    prediction=model.predict(X)
+    #print(prediction)
+    if(prediction[0]==0):
+        return 'Real News'
+    else:
+        return 'Fake News'
+
+if __name__=="__main__":
+    app.run(debug=True, port=5002)
